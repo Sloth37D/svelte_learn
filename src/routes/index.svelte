d3e2e6cbfe0fa25cd3c77c16d4a2923d974525f1
Sloth37D <screamingsloth37d@gmail.com>
2022-05-27 18:19:14 +0900

    1st commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<title>しにゃ</title>
+<h1 class="text-xl">かかって来いって</h1>
+<a href="https://www.disneyplus.com/ja-jp/home" target="_blank" >新井</a><br>
+<a href="/page2" class="text-red-600">Page2</a>
+<script>
+
+</script>
+<style>
+    h1{
+        background-color: black;
+        color: chartreuse;
+    }
+</style>
---
+++ src/routes/page2.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-9xl text-green-300">ページ２</h1>
+<a href="/">戻る</a>
